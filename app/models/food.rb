c53ee9de318340e448c3caf6567c7c05cea01642
masatora <atarashi.masatora@gmail.com>
2020-09-10 17:09:04 +0900

    Implement basic search function

--- app/models/food.rb
+++ app/models/food.rb
@@ -8,16 +8,20 @@ class Food < ApplicationRecord
         similer_foods << candidate_food
       end
     end
+    similer_foods
   end
 
   private
 
   def similer?(food1:, food2:)
     score = calculate_nutrition_score(food1: food1, food2: food2)
-    score <= 20
+    score <= 0.7
   end
 
   def calculate_nutrition_score(food1:, food2:)
-    return 20
+    abs_protein = (food1.protein - food2.protein).abs
+    abs_fat = (food1.fat - food2.fat).abs
+    abs_carbohydrate = (food1.carbohydrate - food2.carbohydrate).abs
+    sum_of_abs = [abs_protein, abs_fat, abs_carbohydrate].sum
   end
 end
