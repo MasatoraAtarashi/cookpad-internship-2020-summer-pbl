91d86eaa6f11838bb990b17bbf60a3545af7ea47
masatora <atarashi.masatora@gmail.com>
2020-09-11 01:38:06 +0900

    Implement search function

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,8 +1,8 @@
 class CartController < ApplicationController
   def index
     @foods = []
-    if saved_food_ids = cookies[:saved_food_ids]
-      saved_food_ids.split(',').map(&:to_i).uniq.each do |id|
+    if food_ids = split_saved_food_ids_to_food_ids(saved_food_ids: cookies[:saved_food_ids])
+      food_ids.each do |id|
         @foods << Food.find(id)
       end
     end
@@ -26,6 +26,25 @@ class CartController < ApplicationController
   end
 
   def search
+    cookpad_base_url = 'https://cookpad.com/search/'
+    search_url = cookpad_base_url
+    if food_ids = split_saved_food_ids_to_food_ids(saved_food_ids: cookies[:saved_food_ids])
+      food_ids.each_with_index do |id, idx|
+        food_name = Food.find(id).name
+        if idx == 0
+          query = food_name
+        else
+          query = "%20" + food_name
+        end
+        search_url += query
+      end
+    end
+    redirect_to search_url
+  end
 
+  private
+
+  def split_saved_food_ids_to_food_ids(saved_food_ids:)
+    saved_food_ids.split(',').map(&:to_i).uniq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :foods, only: %w[index show]
   resources :cart, only: %w[index create]
-  delete "/cart", to: 'cart#destroy'
+  delete '/cart', to: 'cart#destroy'
+  post '/cart/search', to: 'cart#search'
   root to: 'top#index'
 end
