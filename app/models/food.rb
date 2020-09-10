71d3173e248fcc0320fc3d165d529f17d4cf262a
masatora <atarashi.masatora@gmail.com>
2020-09-10 22:31:33 +0900

    Change to display similar ingredients in similar order

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,27 +1,26 @@
 class Food < ApplicationRecord
-  def return_similer_foods
-    similer_foods = []
-    Food.all.find_each do |candidate_food|
-      next if self.id == candidate_food.id
+  # TODO: validation
 
-      if similer?(food1: self, food2: candidate_food)
-        similer_foods << candidate_food
+  def return_similer_foods
+    similer_foods = {}
+    Food.all.find_each do |candidate_food|
+      # TODO: 自分自身を表示すべきか？
+      # next if self.id == candidate_food.id
+      score = calculate_nutrition_score(food1: self, food2: candidate_food)
+      if score <= 100
+        similer_foods[score] = candidate_food
       end
     end
-    similer_foods
+    p similer_foods.sort.to_h.keys
+    similer_foods.sort.to_h.values
   end
 
   private
 
-  def similer?(food1:, food2:)
-    score = calculate_nutrition_score(food1: food1, food2: food2)
-    score <= 0.7
-  end
-
   def calculate_nutrition_score(food1:, food2:)
     abs_protein = (food1.protein - food2.protein).abs
-    abs_fat = (food1.fat - food2.fat).abs
+    abs_lipid = (food1.lipid - food2.lipid).abs
     abs_carbohydrate = (food1.carbohydrate - food2.carbohydrate).abs
-    sum_of_abs = [abs_protein, abs_fat, abs_carbohydrate].sum
+    sum_of_abs = [abs_protein, abs_lipid, abs_carbohydrate].sum
   end
 end
