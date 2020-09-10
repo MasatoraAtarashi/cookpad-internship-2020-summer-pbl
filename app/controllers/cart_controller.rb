e507a09bf8fbd4436fe8b5f7c1b835654cd21033
masatora <atarashi.masatora@gmail.com>
2020-09-11 00:19:32 +0900

    Modify cart functions

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,24 +1,25 @@
 class CartController < ApplicationController
   def index
     @foods = []
-    if saved_food_ids = [cookies[:saved_food_ids]]
-      saved_food_ids.each do |id|
-        # なぜかidが&10として保存されており、to_iすると0になってしまうのでこういう処理をした。
-        @foods << Food.find(id.slice(1..-1).to_i)
+    if saved_food_ids = cookies[:saved_food_ids]
+      saved_food_ids.split(',').map(&:to_i).uniq.each do |id|
+        @foods << Food.find(id)
       end
     end
+    @foods
   end
 
   def create
     # TODO: ajaxでページ遷移しないようにする？←必須じゃないっぽい
-    saved_food_ids = [cookies[:saved_food_ids]] || []
-    cookies.permanent[:saved_food_ids] = saved_food_ids.append(params[:id])
+    saved_food_ids = cookies[:saved_food_ids] || ""
+    cookies.permanent[:saved_food_ids] = saved_food_ids + params[:id] + ","
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     # 全削除か、指定されたやつ削除かわける
     cookies.delete :saved_food_ids
+    redirect_to cart_index_path
   end
 
   def search
