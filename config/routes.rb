0f4f2c5809bff7b6b35b869e8af6e1f9a89b5f56
masatora <atarashi.masatora@gmail.com>
2020-09-10 23:57:48 +0900

    Create a outline of the cart function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :foods, only: %w[index show]
   resources :cart, only: %w[index create]
+  delete "/cart", to: 'cart#destroy'
   root to: 'top#index'
 end
