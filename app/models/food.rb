011bbd82ffb49ada43eeba49f581e573c2450aa7
masatora <atarashi.masatora@gmail.com>
2020-09-11 01:44:55 +0900

    Modify standard score for searching similer foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,17 +1,31 @@
 class Food < ApplicationRecord
   # TODO: validation
 
+  STANDARD_SCORE = 5.freeze
+
   def return_similer_foods
     similer_foods = {}
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
     Food.all.find_each do |candidate_food|
       # TODO: 自分自身を表示すべきか？
       # next if self.id == candidate_food.id
       score = calculate_nutrition_score(food1: self, food2: candidate_food)
-      if score <= 100
+      p score
+      if score <= STANDARD_SCORE
         similer_foods[score] = candidate_food
       end
     end
-    p similer_foods.sort.to_h.keys
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
+    puts 'negitoro'
     similer_foods.sort.to_h.values
   end
 
