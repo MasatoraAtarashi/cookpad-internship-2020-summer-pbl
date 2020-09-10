cfdac79d486f22a338498d4f12f384e5f2f41ccd
masatora <atarashi.masatora@gmail.com>
2020-09-10 16:00:33 +0900

    Implement search function using dummy logic

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,9 @@
 class TopController < ApplicationController
   def index
-    # TODO 検索結果のみを返すようにする
-    @foods = Food.all
+    if @food = Food.find_by(name: params[:query])
+      @foods = @food.return_similer_foods
+    else
+      # TODO 検索した食材が存在しないことをユーザーに伝える
+    end
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,22 @@
 class Food < ApplicationRecord
+  def return_similer_foods
+    similer_foods = []
+    Food.all.each do |candidate_food|
+      next if self.id == candidate_food.id
+      if similer?(food1: self, food2: candidate_food)
+        similer_foods << candidate_food
+      end
+    end
+  end
+
+  private
+
+  def similer?(food1:, food2:)
+    score = calculate_nutrition_score(food1: food1, food2: food2)
+    score <= 20
+  end
+
+  def calculate_nutrition_score(food1:, food2:)
+    return 20
+  end
 end
